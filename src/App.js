a82f248fffbf24a940440abd3f0f8c698d45a2d1
kaarthikrishna <kgk300000@gmail.com>
2023-03-30 11:41:57 +0530

    made some changes in edit option

--- src/App.js
+++ src/App.js
@@ -4,14 +4,11 @@ import Header from './components/Header';
 import Form from './components/Form';
 import TodoLists from './components/TodoLists';
 function App() {
-  const initialState= JSON.parse(localStorage.getItem("todos")) || [];
   const[input,setInput]=useState("");
-  const[todos,setTodos]=useState([initialState]);
+  const[todos,setTodos]=useState([]);
   const[editTodo,setEditTodo]=useState(null);
 
-  useEffect(()=>{
-    localStorage.setItem("todos",JSON.stringify(todos))
-  },[todos])
+ 
   return (
     <div className='container'>
       <div className='app-wrapper'>
